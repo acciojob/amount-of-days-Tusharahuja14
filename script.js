57c74062fdfad7e163fd3b6b8b57595b0460dc91
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2024-12-07 11:24:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysofAYear(const valinput) {
+	if(valinput%366==0)
+	{
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
+const valinput=prompt("Enter value");
+alert(daysofAYear(valinput));
